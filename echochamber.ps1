65d2f03a1f0aad39c034c28d5fbc07cb1776216e
Flamesofphx <flamesofphx@flamesofphx.com>
2022-02-20 10:12:36 -0700

    Add PS1
    Add PS1 echo chamber for poor souls who have to program/script in winslow

---
+++ echochamber.ps1
@@ -0,0 +1,3 @@
+#Oh look some sad soul has to program/script in winslow.. 
+Write-Host "This is how lame windows scripting is" -ForegroundColor black -BackgroundColor White
+#Notice the single dash lazyiness for command options... GEEZ... guess it's not as bad as programmers leveling the "Ab" off on the word "user" all the time.
